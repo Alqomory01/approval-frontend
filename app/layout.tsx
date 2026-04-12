fb7427737b1069c3e5ae5b3dfadd2071866726d9
Alqomory01 <Alqomorytech@gmail.com>
2026-04-12 18:20:21 +0100

    new dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Sidebar from "./components/Sidebar";
+// import Sidebar from "./components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <div className="flex flex-1">
-      <Sidebar/>
+      {/* <Sidebar/> */}
         <main className="flex-1 p-6 bg-gray-100">{children}</main>
         </div>
         </body>
