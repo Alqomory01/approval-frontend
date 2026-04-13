a801f82a897f4810f845fa63b6007991a89ce4dd
Alqomory01 <Alqomorytech@gmail.com>
2026-04-13 15:19:46 +0100

    new features added

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -12,6 +12,20 @@ export default function Card({ data }:Props) {
     <div className="bg-white shadow-md rounded-xl p-4 space-y-2">
       <h2 className="font-semibold">{data.title}</h2>
       <p>₦{data.amount}</p>
+      <div className="border-t pt-3 text-sm space-y-1 text-gray-700">
+          <h3>Staff Info</h3>
+        <p><span className="font-semibold">Staff Name:</span> {data.staffName}</p>
+
+        <p><span className="font-semibold">Department:</span> {data.department}</p>
+
+        <p><span className="font-semibold">Function:</span> {data.function}</p>
+
+        <p><span className="font-semibold">Job Title:</span> {data.jobTitle}</p>
+
+        <p><span className="font-semibold">Job Level:</span> {data.jobLevel}</p>
+
+      </div>
+
       <StatusBadge status={data.status} />
     </div>
   );
